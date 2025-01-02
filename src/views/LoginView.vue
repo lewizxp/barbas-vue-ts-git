28adfbc9b39b989d1cb01e3a6f5e1f4e15669be9
fMoreira <fabianomoreira.ti@gmail.com>
2025-01-01 22:04:56 -0300

    refactor: ajuste de configurações

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -23,7 +23,7 @@
       </div>
 
       <div class="form-group-inline">
-        <input type="checkbox" name="remenber" id="rem" />
+        <input type="checkbox" name="remember" id="rem" />
         <label for="rem">Lembre-me</label>
       </div>
 
