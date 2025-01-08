1e616e701243f60d2fa51bb88fa2ed7bf2495b26
fMoreira <fabianomoreira.prof@gmail.com>
2025-01-07 21:55:52 -0300

    feat: add person

--- src/core/infra/firebase.ts
+++ src/core/infra/firebase.ts
@@ -1,3 +1,10 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
@@ -9,3 +16,7 @@ export const firebaseConfig = {
     appId: "1:800838594148:web:341626557847e3173c7971",
     measurementId: "G-7BN122WWNX",
 };
+
+// Initialize Firebase ---------------------------
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
