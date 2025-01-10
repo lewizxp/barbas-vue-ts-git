b0976abdd663a2ca4741bee31a8d1d357b4cc4aa
fMoreira <fabianomoreira.prof@gmail.com>
2025-01-09 21:48:48 -0300

    feat: add person

---
+++ src/components/InputValue.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="mb-3">
+        <label for="nameInput" class="form-label">Nome</label>
+        <input
+            type="text"
+            class="form-control"
+            id="nameInput"
+            aria-describedby="nameHelp"
+            name="nome"
+            required
+            v-model="value"
+            @input="
+                () => {
+                    isValidate = isValue(value);
+                }
+            "
+        />
+        <div id="nameHelp" class="form-text">
+            <span v-if="!isValidate"> Nome invalido</span>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+import { isValue } from "@/core/helpers/validator";
+
+defineProps({
+    value: {
+        type: [String, Number],
+        default: undefined,
+    },
+});
+
+const isValidate = ref(true);
+</script>
+
+<style scoped></style>
