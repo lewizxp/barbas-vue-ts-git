79466ecec3992deb0de3efb4c0922f66d453a486
fmoreira <fabianomoreira.prof@gmail.com>
2025-01-28 21:51:37 -0300

    dados do usuario

--- src/core/service/auth.service.ts
+++ src/core/service/auth.service.ts
@@ -1,16 +1,21 @@
-import { getLoginGoogle, postLoginEmail } from "@/core/infra/auth.repository";
+import {
+    auth,
+    getLoginGoogle,
+    postLoginEmail,
+} from "@/core/infra/auth.repository";
 import { Person } from "../domain/Person";
 
 export const authService = {
     loginEmail,
     loginGoogle,
+    getAuthUser,
 };
 
 async function loginEmail(email: string, password: string) {
     return await postLoginEmail(email, password)
         .then(res => {
             const user = res.user;
-            return <Person>{
+            const person = <Person>{
                 email: user.email,
                 name: user.displayName,
                 photo: user.photoURL,
@@ -36,4 +41,14 @@ async function loginGoogle() {
             console.error(">>> error:", error);
             throw new Error("Email ou senha inválidos!");
         });
+}
+
+function getAuthUser() {
+    console.log(auth.currentUser);
+    const user = auth.currentUser;
+    return <Person>{
+        email: user?.email,
+        name: user?.displayName,
+        photo: user?.photoURL,
+    };
 }
